f9d7beebec693abeb122a82b1552a3b7c3d27391
mohitrohilla22 <151056375+mohitrohilla22@users.noreply.github.com>
2024-07-27 00:07:13 +0530

    final commit

--- celeryconfig.py
+++ celeryconfig.py
@@ -1,8 +1,8 @@
 from pydantic_settings import BaseSettings
 
 class Settings(BaseSettings):
-    DATABASE_URL: str = "sqlite:///./test.db"
-    CELERY_BROKER_URL: str = "redis://localhost:6379/0"
-    CELERY_RESULT_BACKEND: str = "redis://localhost:6379/0"
+    DATABASE_URL: str = 'mysql://sql12722367:73DvxQuh72@sql12.freesqldatabase.com:3306/sql12722367'
+    CELERY_BROKER_URL: str = "redis-14766.c98.us-east-1-4.ec2.redns.redis-cloud.com:14766"
+    CELERY_RESULT_BACKEND: str = "redis-14766.c98.us-east-1-4.ec2.redns.redis-cloud.com:14766"
 
 settings = Settings()
